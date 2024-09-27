4c9c8b421991bb7064009086fee76429ca030c91
Celal Albayrak <celalce25@hotmail.com>
2024-09-28 00:02:18 +0300

    yasamdongusu  added

---
+++ app/src/main/java/com/celalalbayrak/yasamdongusu/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.celalalbayrak.yasamdongusu
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.celalalbayrak.yasamdongusu.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        enableEdgeToEdge()
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.textView.text = "Merhaba Dünya"
+
+    }
+    fun kaydet(view : View) {
+        binding.textView.text = "Kayıt Edildi!"
+    }
+
+    fun iptal(view : View) {
+        binding.textView.text = "İptal Edildi!"
+
+    }
+    fun sonrakisayfa(view : View) {
+val intent = Intent(this,SecondActivity::class.java)
+        startActivity(intent)
+    }
+
+}
